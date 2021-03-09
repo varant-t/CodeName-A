17bdaf844850fc2c995bf343c7ba89e7cf0ae267
varant-t <vv_titizian@hotmail.ca>
2021-03-09 09:58:13 -0500

    Removed Code / Changed Level
    -Removed the code we werent using and the comments with old code
    -Added new platforms to start as a tutorial or starting area

--- CodeName-A/Assets/Scripts/PlayerMovement.cs
+++ CodeName-A/Assets/Scripts/PlayerMovement.cs
@@ -18,29 +18,20 @@ public class PlayerMovement: MonoBehaviour
     public GameObject[] phasedObjects;
     public GameObject[] phasedObjs;
 
-    // Testing with GameObjects
-    public GameObject platform1;
-    public GameObject platform2;
     public bool phased;
     public SpriteRenderer backgroundRenderer;
 
     // Start is called before the first frame update
     void Start()
     {
+        // Initialize that the starting world isnt not phased, and add all objects with Phased tag into array
         phased = false;
-
-        //backgroundRenderer = GetComponent<SpriteRenderer>();
-        //phasedObjects = new GameObject[10];
-        
-        //Debug.Log(phasedObjects.Length);
-        //phasedObjs = new GameObject[100];
         phasedObjs = GameObject.FindGameObjectsWithTag("Phased");
         for (int i = 0; i < phasedObjs.Length; i++)
         {
             phasedObjs[i].SetActive(false);
         }
-        //phasedObjects = new GameObject[tempArray.Length];
-        //phasedObjects.Length
+ 
         anim = GetComponent<Animator>();
         myRigidBody = GetComponent<Rigidbody2D>();
 
@@ -87,20 +78,12 @@ public class PlayerMovement: MonoBehaviour
                 Debug.Log("Hello");
                 phasedObjs[i].SetActive(true);
             }
-            //platform1.SetActive(true);
-            //platform2.SetActive(true);
-            //for (int i = 0; i < phasedObjects.Length; i++)
-            //{
-            //    GameObject gameObjects = phasedObjects[i];
-            //    gameObject.activeInHierarchy.Equals(true);
-            //}
+         
         }
         else if (Input.GetKeyDown(KeyCode.Q) && phased)
         {
             backgroundRenderer.color = new Color(0.120448f, 0.01944643f, 0.1792453f, 1f);
             phased = false;
-            //platform1.SetActive(false);
-            //platform2.SetActive(false);
             for (int i = 0; i < phasedObjs.Length; i++)
             {
                 phasedObjs[i].SetActive(false);
@@ -108,18 +91,10 @@ public class PlayerMovement: MonoBehaviour
         }
 
         if (Input.GetButtonDown("Jump") && isGrounded)
-        {
-            //anim.SetTrigger("isJumping");
-            //anim.SetBool("isGrounded", false);
-            
+        { 
             myRigidBody.velocity = Vector2.zero;
-
             myRigidBody.AddForce(Vector2.up * jumpForce);
-
-
             Debug.Log("Jump");
-
-           // anim.ResetTrigger("isJumping");
         }
 
       
