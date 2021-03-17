3aa73077a7000a539c0a0e3573e6039b353402f6
Matt <bendermaj@gmail.com>
2021-03-16 23:49:21 -0400

    AI returns to patrol
    -AI when not between patrol points will return to patrol points
    -Also changed ai rotation based on direction its moving in

--- CodeName-A/Assets/EnemyAI.cs
+++ CodeName-A/Assets/EnemyAI.cs
@@ -11,6 +11,8 @@ public class EnemyAI : MonoBehaviour
     private bool goingLeft = true;
     private bool moving = true;
     private int constantDirection = -1;
+    public GameObject patrolNode1;
+    public GameObject patrolNode2;
 
     // Start is called before the first frame update
     void Start()
@@ -24,16 +26,19 @@ public class EnemyAI : MonoBehaviour
     void Update()
     {
         int moveHorizontal = 0;
+        OutsidePatrolBounds();
         if (moving)
         {
             enemyAnim.SetInteger("AnimState", 2);
             if (goingLeft)
             {
                 moveHorizontal = -1;
+                transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
             }
             else
             {
                 moveHorizontal = 1;
+                transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
             }
         }
         else
@@ -56,7 +61,22 @@ public class EnemyAI : MonoBehaviour
     private void ContinuePatrol()
     {
         constantDirection = -constantDirection;
-        transform.localScale = new Vector3(-transform.localScale.x, 1.0f, 1.0f);
+        
         moving = true;
+    }
+    private void OutsidePatrolBounds()
+    {
+        if(transform.position.x > patrolNode1.transform.position.x && transform.position.x > patrolNode2.transform.position.x)
+        {
+            moving = true;
+            goingLeft = true;
+            transform.localScale = new Vector3(-transform.localScale.x, 1.0f, 1.0f);
+        }
+        else if (transform.position.x < patrolNode1.transform.position.x && transform.position.x < patrolNode2.transform.position.x)
+        {
+            moving = true;
+            goingLeft = false;
+            transform.localScale = new Vector3(-transform.localScale.x, 1.0f, 1.0f);
+        }
     }
 }
